dee3619077502308094be4cd3984970af697b238
Valentine-Efagene <efagenevalentine@gmail.com>
2020-04-14 04:25:36 +0100

    chapter9 in progress

---
+++ chapter9/ui/src/IssueFilter.jsx
@@ -0,0 +1,17 @@
+/* eslint "react/prefer-stateless-function": "off" */
+
+import React from 'react';
+
+export default class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>
+        <a href='/#/issues'>All Issues</a>
+        {' | '}
+        <a href='/#/issues?status=New'>New Issues</a>
+        {' | '}
+        <a href='/#/issues?status=Assigned'>Assigned Issues</a>
+      </div>
+    );
+  }
+}
