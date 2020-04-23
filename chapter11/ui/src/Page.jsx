6dd3674f26d37dc519e68747fcf1575ab45c8cab
Valentine-Efagene <efagenevalentine@gmail.com>
2020-04-23 21:03:03 +0100

    Chapter11 Navbar

--- chapter11/ui/src/Page.jsx
+++ chapter11/ui/src/Page.jsx
@@ -1,18 +1,66 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem,
+  Glyphicon,
+  Tooltip,
+  OverlayTrigger,
+} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
 import Contents from './Contents.jsx';
 
 function NavBar() {
   return (
-    <nav>
-      <NavLink exact to='/'>
-        Home
-      </NavLink>
-      {' | '}
-      <NavLink to='/issues'>Issue List</NavLink>
-      {' | '}
-      <NavLink to='/report'>Report</NavLink>
-    </nav>
+    // just enter property fixedTop without argument to fix the footer
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>Issue Tracker</Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <LinkContainer to='/issues'>
+          <NavItem>Issue List</NavItem>
+        </LinkContainer>
+        <LinkContainer to='/report'>
+          <NavItem>Report</NavItem>
+        </LinkContainer>
+      </Nav>
+      <Nav pullRight>
+        <NavItem>
+          <OverlayTrigger
+            placement='left'
+            delayShow={1000}
+            overlay={<Tooltip id='create-issue'>Create Issue</Tooltip>}
+          >
+            <Glyphicon glyph='plus' />
+          </OverlayTrigger>
+        </NavItem>
+        <NavDropdown
+          id='user-dropdown'
+          title={<Glyphicon glyph='option-vertical' />}
+          noCaret
+        >
+          <MenuItem>About</MenuItem>
+        </NavDropdown>
+      </Nav>
+    </Navbar>
+  );
+}
+
+function Footer() {
+  return (
+    <small>
+      <p className='text-center'>
+        Full source code available at this{' '}
+        <a href='https://github.com/Valentine-Efagene/MERN'>
+          Github repository
+        </a>
+      </p>
+    </small>
   );
 }
 
@@ -21,6 +69,7 @@ export default function Page() {
     <div>
       <NavBar />
       <Contents />
+      <Footer />
     </div>
   );
 }
